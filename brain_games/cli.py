d0404811bd81b538caa09c5773b15116fd625a79
nurgeldy <nurgeldy@gmail.com>
2026-03-27 15:38:34 +0500

    deletion of the if name == 'main' block

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -9,8 +9,3 @@ def welcome_user():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     return name
-
-
-if __name__ == '__main__':
-    greet()
-    welcome_user()
