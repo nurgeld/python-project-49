6708e189f333353a92d004315da0348bb12c6229
nurgeldy <nurgeldy@gmail.com>
2026-03-28 19:33:45 +0500

    refacroring

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -4,17 +4,28 @@ import random
 GAME_RULE = 'What number is missing in the progression?'
 
 
+def generate_progression(first, diff, length):
+    return [first + n * diff for n in range(length)]
+
+
+def build_question(progression, hidden_index):
+    parts = []
+    for i, num in enumerate(progression):
+        if i == hidden_index:
+            parts.append('..')
+        else:
+            parts.append(str(num))
+    return ' '.join(parts)
+
+
 def generate_game():
     first = random.randint(-15, 15)
     diff = random.randint(1, 15)
     progression_length = random.randint(5, 15)
-    index_of_masked_element = random.randint(0, progression_length - 1)
+    hidden_index = random.randint(0, progression_length - 1)
 
-    progression = [
-        first + n * diff for n in range(progression_length)
-    ]
-    progression[index_of_masked_element] = '..'
-    question = ' '.join(str(x) for x in progression)
-    answer = str(first + index_of_masked_element * diff)
+    progression = generate_progression(first, diff, progression_length)
+    answer = str(progression[hidden_index])
+    question = build_question(progression, hidden_index)
 
     return (question, answer)
